86b1f716c476b5dd3cf28dc544adba4dc297c2a5
Sanyam Jain <sammy@Sanyams-MacBook-Pro-2.local>
2025-06-21 16:12:17 +0530

    updated code to use ipv4 address for supabase<>railway

--- fastapi_backend/app/db/database.py
+++ fastapi_backend/app/db/database.py
@@ -1,12 +1,25 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-
+# from sqlalchemy.pool import NullPool
 from app.core.config import settings
 
-# Create SQLAlchemy engine
-engine = create_engine(settings.DATABASE_URL)
+# Fetch variables
+USER = settings.USER
+PASSWORD = settings.PASSWORD
+HOST = settings.HOST
+PORT = settings.PORT
+DBNAME = settings.DBNAME
 
+# Import urllib for URL encoding
+from urllib.parse import quote_plus
+from sqlalchemy.pool import NullPool
+
+# Construct the SQLAlchemy connection string with URL-encoded password
+DATABASE_URL = f"postgresql+psycopg2://{USER}:{quote_plus(PASSWORD)}@{HOST}:{PORT}/{DBNAME}?sslmode=require"
+
+# Create the SQLAlchemy engine
+engine = create_engine(DATABASE_URL, poolclass=NullPool)
 # Create SessionLocal class
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
